a4f3d37fb6df31e3b35d3bb457a95d9c27f96a30
lynhuynh <lynhuynh2031@gmail.com>
2016-04-18 20:56:01 -0500

    Added book page for user

--- user/books.php
+++ user/books.php
@@ -63,13 +63,73 @@
     </nav>
     <!-- END OF NAVBAR -->
 
-    <!-- Start your coding below here -->
+    <?php
+    /*There are two parts to connection to the database and querying results, THIS IS STEP 1
+    SEE LINE 133 for further step*/
+    global $DataBaseCon; //grabs connection to MYSQL database
+    //My Query I will be Using
+    $getData = "SELECT book_id ,book_title,author_fname,author_lname,genre,year_ofpub,isbn,language,cost FROM books ORDER BY book_title"; 
+    $results = mysqli_query($DataBaseCon, $getData);  //Grab results from database using connection and query
+?>
+  
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-1 main row-padding">
+            <div class="panel panel-primary">
+                <div class="panel-heading"><h4 class="sub-header text-center">Here is the list of available books in Vunnava Dot Com Library: </h4></div>
+                <div class="panel-body">    <div class="table-responsive">
+                        <table class="table table-striped">
+    
+           <!-- Table with Data -->
+          
+           
+              <!--Headers for data table-->
+              <thead>
+                <tr>
+                  <th>Book Title<</th>
+                  <th>Author Name</th>
+                  <th>Genre</th>
+                  <th>Year of Publish</th>
+                   <th>ISBN</th>
+                  <th>Language</th>
+                  <th>Cost</th>
+                </tr>
+              </thead>
+              <!--Data for Table -->
+              <tbody>
+                <?php
+                  /*THIS IS STEP 2 in QUERYING FROM DATABASE SEE LINE 79 FOR STEP 1*/
+                 
+                  //if data exist in table
+                  if (mysqli_num_rows($results) > 0)
+                  {
+                    //output data of each row 
+                    while ($row = mysqli_fetch_assoc($results))
+                    {
+                      /*while results has row of data. output first name, last name
+                      date, total amount and a link*/
+                
+                      echo "<tbody>";
+        echo "<tr>";
+        echo "<td>" . $row["book_title"] . "</td>";
+        echo "<td>" . $row["author_fname"] . " " . $row["author_lname"] . "</td>";
+        echo "<td>" . $row["genre"] . "</td>";
+        echo "<td>" . $row["year_ofpub"] . "</td>";
+        echo "<td>" . $row["isbn"] . "</td>";
+        echo "<td>" . $row["language"] . "</td>";
+        echo "<td>" . $row["cost"] . "</td>";
+        echo "</tr>";
+        
+        echo "</tbody>";
+                    }
+                  }
+                ?>  
+              </tbody>
+        
 
-
-
-
-
-  </body>
+       
+                        </table>
+                    </div></div>
+            </div>
+        </div>
 <!-- end of body -->
 
   <!-- Bootstrap core JavaScript
